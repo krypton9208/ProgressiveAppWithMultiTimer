154b7f5692bc26b93e77480a7239af9a2c494bc5
Damian Krysta <krypton9208@gmail.com>
2017-08-20 20:46:31 +0200

    EDIT path to service worker

--- scripts/app.js
+++ scripts/app.js
@@ -211,7 +211,7 @@
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register('service-worker.js')
+      .register('ProgressiveAppWithMultiTimer/service-worker.js')
       .then(function() {  });
   }
 
