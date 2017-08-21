7219b5d98628c2d969eef549ab78c0e58da77399
Damian Krysta <krypton9208@gmail.com>
2017-08-21 19:19:45 +0200

    EDIT refactor methods to localStorage

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@
 
   var app = {
     isLoading: true,
+    localStorage: {},
     visibleClients: {},
     currentClientsList: [],
     timers: [],
@@ -54,19 +55,18 @@
   app.updateClientCard = function(data) {
     var id = data.id;
     var name = data.name;
-    var created = data.created;
     var totalSeconds = data.totalTime;
 
     var client = app.visibleClients[id];
     if (!client) {
       client = app.timerTemplate.cloneNode(true);
       client.classList.remove('timerTemplate');
-      client.querySelector('.timerTimeHours').textContent = pad(parseInt(totalSeconds/3600));
-      client.querySelector('.timerTimeMinutes').textContent = pad(parseInt(totalSeconds/60));
-      client.querySelector('.timerTimeSeconds').textContent = pad(totalSeconds%60);
+      client.querySelector('.timerTimeHours').textContent = app.pad(parseInt(totalSeconds/3600));
+      client.querySelector('.timerTimeMinutes').textContent = app.pad(parseInt(totalSeconds/60));
+      client.querySelector('.timerTimeSeconds').textContent = app.pad(totalSeconds%60);
       client.querySelector('.timerStart').onclick = function () { app.startTimer(id) };
-      client.querySelector('.timerEnd').onclick = function () { app.endTimer(id, parseInt(client.querySelector('.timerTimeHours').textContent * 3600 + client.querySelector('.timerTimeMinutes').textContent * 60 + client.querySelector('.timerTimeSeconds').textContent)) };
-      client.querySelector('.timerRemove').onclick = function () { app.removeTimer(id)  }
+      client.querySelector('.timerEnd').onclick = function () { app.endTimer(id, app.totalSeconds(parseInt(client.querySelector('.timerTimeHours').textContent), parseInt(client.querySelector('.timerTimeMinutes').textContent),  parseInt(client.querySelector('.timerTimeSeconds').textContent))) };
+      client.querySelector('.timerRemove').onclick = function () { app.removeTimer(id) };
       client.setAttribute('id', id);
       client.querySelector('#name').textContent = name ;
       client.removeAttribute('hidden');
@@ -74,6 +74,10 @@
       app.visibleClients[id] = client;
     }
 
+  };
+
+  app.totalSeconds = function (hours, minutes, seconds) {
+    return (hours * 3600) + Math.floor(minutes * 60) + seconds
   };
 
   app.startNewTimerForClient = function (key, totalSeconds) {
@@ -84,9 +88,9 @@
     var i = totalSeconds;
     app.timers[key] = setInterval( function updateText() {
       i++;
-      clientContainer.querySelector('.timerTimeHours').textContent = pad(parseInt(i/3600));
-      clientContainer.querySelector('.timerTimeMinutes').textContent = pad(parseInt(i/60));
-      clientContainer.querySelector('.timerTimeSeconds').textContent = pad(i%60);
+      clientContainer.querySelector('.timerTimeHours').textContent = app.pad(parseInt(i/3600));
+      clientContainer.querySelector('.timerTimeMinutes').textContent = app.pad(parseInt(i/60));
+      clientContainer.querySelector('.timerTimeSeconds').textContent = app.pad(i%60);
     }, 1000);
   };
 
@@ -96,10 +100,9 @@
     clientContainer.querySelector('.timerStart').classList.remove('hidden');
     clientContainer.querySelector('.timerEnd').classList.add('hidden');
     clientContainer.querySelector('#running').classList.add('hidden');
-    clientContainer.querySelector('.timerTimeHours').textContent = pad(parseInt(totalTime/3600));
-    clientContainer.querySelector('.timerTimeMinutes').textContent = pad(parseInt(totalTime/60));
-    clientContainer.querySelector('.timerTimeSeconds').textContent = pad(totalTime%60);
-
+    clientContainer.querySelector('.timerTimeHours').textContent = app.pad(parseInt(totalTime/3600));
+    clientContainer.querySelector('.timerTimeMinutes').textContent = app.pad(parseInt(totalTime/60));
+    clientContainer.querySelector('.timerTimeSeconds').textContent = app.pad(totalTime%60);
   };
 
   app.removeTimerForClient = function (key) {
@@ -122,20 +125,7 @@
     app.isLoading = value;
   };
 
-  function pad(val)
-  {
-    var valString = val + "";
-    if(valString.length < 2)
-    {
-      return "0" + valString;
-    }
-    else
-    {
-      return valString;
-    }
-  }
-
-  /*****************************************************************************
+    /*****************************************************************************
    *
    * Methods for dealing with the model
    *
@@ -151,15 +141,14 @@
 
     app.currentClientsList.push(key);
 
-    // TODO add cache logic here
-    window.localStorage.setItem(key, JSON.stringify(client));
-    window.localStorage.setItem('currentClientsList', JSON.stringify(app.currentClientsList));
+    app.localStorage.saveSingleClient(key, client);
+    app.localStorage.saveCurrentClientList();
 
     app.updateClientCard(client)
   };
 
  app.startTimer = function (key) {
-   var client = JSON.parse(window.localStorage.getItem(key));
+   var client = app.localStorage.getSingleClient(key);
 
    if (!client.timerSessions) client.timerSessions = [];
    client.timerSessions.push({
@@ -167,20 +156,18 @@
      endTimer: null
    });
 
-   window.localStorage.removeItem(key);
-   window.localStorage.setItem(key, JSON.stringify(client));
+   app.localStorage.saveSingleClient(key, client)
 
    app.startNewTimerForClient(key, client.totalTime);
  };
 
   app.endTimer = function (key, totalSeconds) {
-    var client = JSON.parse(window.localStorage.getItem(key));
+    var client = app.localStorage.getSingleClient(key);
 
-    client.timerSessions[client.timerSessions.length -1 ].endTimer = new Date();
+    client.timerSessions[client.timerSessions.length -1].endTimer = new Date();
     client.totalTime = totalSeconds;
 
-    window.localStorage.removeItem(key);
-    window.localStorage.setItem(key, JSON.stringify(client));
+    app.localStorage.saveSingleClient(key, client)
 
     app.endTimerForClient(key, client.totalTime);
   };
@@ -188,22 +175,35 @@
   app.removeTimer = function (key) {
     app.updateLoading(false);
 
-    app.currentClientsList.splice(app.currentClientsList.indexOf(key), 1)
+    app.currentClientsList.splice(app.currentClientsList.indexOf(key), 1);
 
-    window.localStorage.removeItem(key);
-    window.localStorage.setItem('currentClientsList', JSON.stringify(app.currentClientsList));
+    app.localStorage.removeSingleClient(key);
+    app.localStorage.saveCurrentClientList();
 
     app.removeTimerForClient(key)
   };
 
-  app.currentClientsList = JSON.parse(window.localStorage.getItem('currentClientsList'));
+  app.pad = function pad(val) {
+    var valString = val + "";
+    if(valString.length < 2)
+    {
+      return "0" + valString;
+    }
+    else
+    {
+      return valString;
+    }
+  };
+
+  app.currentClientsList = app.localStorage.getCurrentClientList();
   if (!app.currentClientsList) {
     app.currentClientsList = [];
     app.updateLoading(true);
   } else {
     app.currentClientsList.forEach( function (client) {
-      if (JSON.parse(window.localStorage.getItem(client))) {
-        app.updateClientCard(JSON.parse(window.localStorage.getItem(client)))
+      var value = app.localStorage.getSingleClient(client)
+      if (value) {
+        app.updateClientCard(app.localStorage.getSingleClient(value))
       }
     });
     app.updateLoading(true);
@@ -214,5 +214,32 @@
       .register('service-worker.js')
       .then(function() {  });
   }
+
+  /*****************************************************************************
+   *
+   * Methods for dealing with localStorage
+   *
+   ****************************************************************************/
+
+  app.localStorage.getSingleClient = function (key) {
+    return JSON.parse(window.localStorage.getItem(key))
+  };
+
+  app.localStorage.saveSingleClient = function (key, client) {
+    window.localStorage.setItem(key, JSON.stringify(client));
+  };
+
+  app.localStorage.removeSingleClient = function (key) {
+    window.localStorage.removeItem(key);
+  };
+
+  app.localStorage.getCurrentClientList = function () {
+    return JSON.parse(window.localStorage.getItem('currentClientsList'))
+  };
+
+  app.localStorage.saveCurrentClientList = function () {
+    window.localStorage.setItem('currentClientsList', JSON.stringify(app.currentClientsList));
+  };
+
 
 })();
